dbae92a253cd10a1b5760e3040f1f4777c4b3508
Nathan Steele <9lf7wmbn@students.codeinstitute.net>
2024-10-16 22:31:36 +0000

    Added feedback for when users guess is correct and incorrect

--- battleships.py
+++ battleships.py
@@ -1,3 +1,4 @@
+import random
 
 board = []
 
@@ -10,24 +11,24 @@ def print_board(board):
     
 
     
-def guess_row(board):
-    return 0, len(board) - 1
+def generate_guess():
+    return random.randint(0, 9), random.randint(0, 9)
 
-    guess_user_row = guess_row(board)
-
-def guess_column(board):
-    return 0, len(board[0]) - 1
-
-    guess_user_column = guess_column(board)
-    
 def guess_location():   
-    user_column_guess = input("Enter a Column Number: ")
-    user_row_guess = input("Enter a Row Number: ")
+    user_guess = generate_guess()
     
+    user_column_guess = int(input("Enter a Column Number: "))
+    user_row_guess = int(input("Enter a Row Number: "))
 
-    
+def validate_location_guess(guess_location):
+    if guess_location == generate_guess:
+        print("Battleship has been sunk, Congratulations!") 
+    else:
+        print("No hit! Better luck next time.")
+        
 def main():
     print_board(board)
     guess_location()
+    validate_location_guess(guess_location)
      
 main()
