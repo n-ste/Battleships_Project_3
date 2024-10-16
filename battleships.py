6fe55e1dbf46535da82e7e5c18da9d047ac43f34
Nathan Steele <9lf7wmbn@students.codeinstitute.net>
2024-10-16 21:40:36 +0000

    Added function for user to guess location on battleships

--- battleships.py
+++ battleships.py
@@ -1,14 +1,33 @@
 
 board = []
 
-for i in range(0,10):
+for space in range(0,10):
     board.append(["."] * 10)
 
 def print_board(board):
-    for i in board:
-        print(" ".join(i))
+    for space in board:
+        print(" ".join(space))
+    
+
+    
+def guess_row(board):
+    return 0, len(board) - 1
+
+    guess_user_row = guess_row(board)
+
+def guess_column(board):
+    return 0, len(board[0]) - 1
+
+    guess_user_column = guess_column(board)
+    
+def guess_location():   
+    user_column_guess = input("Enter a Column Number: ")
+    user_row_guess = input("Enter a Row Number: ")
+    
+
     
 def main():
     print_board(board)
-    
+    guess_location()
+     
 main()
