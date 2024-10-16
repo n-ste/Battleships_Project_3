59077d261cfe08afd8bb590caddbb59eeaf08aac
Nathan Steele <9lf7wmbn@students.codeinstitute.net>
2024-10-16 23:13:06 +0000

    minor amendments made to functions to display values of ship to hit and displaying the board after the attempt

--- battleships.py
+++ battleships.py
@@ -9,12 +9,11 @@ def print_board(board):
     for space in board:
         print(" ".join(space))
     
-
-    
 def generate_guess():
     return random.randint(0, 5), random.randint(0, 5)
 
 def guess_location():   
+    print(generate_guess())
     user_column_guess = int(input("Enter a Column Number: "))
     user_row_guess = int(input("Enter a Row Number: "))
     return user_column_guess,user_column_guess
@@ -22,11 +21,11 @@ def guess_location():
 def validate_location_guess(user_guess, computer_location):
     if user_guess == computer_location:
         print("Battleship has been sunk, Congratulations!")
-        print_board(board) 
+        board[user_guess[0]][user_guess[1]] = "/"
     else:
         print("No hit! Better luck next time.")
         board[user_guess[0]][user_guess[1]] = "X"
-        print_board(board)
+    print_board(board)
         
 def main():
     computer_location = generate_guess()
