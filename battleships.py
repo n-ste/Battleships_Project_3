66c1c91c911e7a94aaad88ac5f2edc3fc88a6fbc
Nathan Steele <9lf7wmbn@students.codeinstitute.net>
2024-10-18 01:27:11 +0000

    Loop added to allow the user to have 5 guesses and update the board with each guess

--- battleships.py
+++ battleships.py
@@ -1,6 +1,16 @@
 # Imported the random library to help in creating a randon integer for computer
 import random
 
+# Introduction message
+def welcome():
+    print("Hello and welcome to my Battleships game!...")
+    print(" ")
+    print("Each player will have 5 ships across a 5X5 board.")
+    print(" ")
+    print("You will have 5 guesses to guess the location of the computer ships!")
+    print(" ")
+    print("Ready.. set.. GO!")
+    print(" ")
 # Empty array for the player board
 board = []
 
@@ -23,7 +33,7 @@ def generate_column_guess():
     
 computer_row = generate_row_guess()
 computer_column = generate_column_guess()
-
+    
 # Displays the computers row and column values for validation purposes
 def guess_location():   
     print(f"Computer's guess location: {computer_row}, {computer_column}")
@@ -43,20 +53,36 @@ def guess_entry():
 
 # Provides validation for the users guess of the computers ships
 def validate_location_guess(user_row_guess, user_column_guess):
+    if board[user_row_guess][user_column_guess] in ["X", "/"]:
+        print("You've already guessed that location! Try again.")
+        
     if user_row_guess == computer_row and user_column_guess == computer_column:
         print("Battleship has been sunk, Congratulations!")
         board[user_row_guess][user_column_guess] = "/"
     else:
         print("No hit! Better luck next time.")
         board[user_row_guess][user_column_guess] = "X"
-        
+            
     print_board(board)
-
+    
+# Creates the loop so the player can play and updates the board after each play    
+def game_over():
+    game_over = False
+    attempts = 0
+    max_attempts = 4
+    while not game_over and attempts < max_attempts:
+        guess_location()
+        user_row_guess, user_column_guess = guess_entry()
+        game_over = validate_location_guess(user_row_guess, user_column_guess)
+        attempts += 1  
+        
 # Functions added to main for easier log of called functions
 def main():
+    welcome()
     guess_location()
     user_row_guess, user_column_guess = guess_entry()
     validate_location_guess(user_row_guess, user_column_guess)
+    game_over()
 
 # Calls main function
 if __name__ == "__main__":     
