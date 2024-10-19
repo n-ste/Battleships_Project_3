b5ec5746fc4eec7076f995255be412c23b106798
Nathan Steele <9lf7wmbn@students.codeinstitute.net>
2024-10-19 11:21:47 +0000

    Encapsulated code into a class and refactored to work

--- battleships.py
+++ battleships.py
@@ -1,96 +1,107 @@
 # Imported the random library to help in creating a randon integer for computer
 import random
 
-# Introduction message
-def welcome():
-    print("Hello and welcome to my Battleships game!...")
-    print(" ")
-    print("Each player will have 5 ships across a 5X5 board.")
-    print(" ")
-    print("You will have 5 guesses to guess the location of the computer ships!")
-    print(" ")
-    print("Ready.. set.. GO!")
-    print(" ")
-# Empty array for the player board
-board = []
-
-# Initialize the board
-for space in range(0, 5):
-    board.append(["."] * 5)
-
-# Prints the board and removes square brackets
-def print_board(board):
-    for space in board:
-        print(" ".join(space))
-
-# Computer generates a row value        
-def generate_row_guess():
-    return random.randint(0, 4)
-
-# Computer generates a column value       
-def generate_column_guess():
-    return random.randint(0, 4)
-    
-computer_row = generate_row_guess()
-computer_column = generate_column_guess()
-
-# Displays the computers row and column values for validation purposes
-def guess_location():   
-    print(f"Computer's guess location: {computer_row}, {computer_column}")
-
-# Instructs the user to input their guess for the location of the computers ships
-def guess_entry():
-    while True:
-        try:
-            user_row_guess = int(input("Enter a Row Number: "))
-            user_column_guess = int(input("Enter a Column Number: "))
-            if 0 <= user_row_guess <= 4 and 0 <= user_column_guess <= 4:
-                return user_row_guess, user_column_guess
-            else:
-                print("Please enter numbers between 0 and 4.")
-        except ValueError:
-            print("Invalid input. Please enter integers only.")
-
-# Provides validation for the users guess of the computers ships
-def validate_location_guess(user_row_guess, user_column_guess):
-    
-    # If user guesses the same coordinates more than once they'll be met with an message
-    if board[user_row_guess][user_column_guess] in ["X", "/"]:
-        print("You've already guessed that location! Try again.")
-    
-    # If the user guesses the correct coordiates of the ship they'll be met with a message advising of this with an "/" displayed on the board
-    if user_row_guess == computer_row and user_column_guess == computer_column:
-        print("Battleship has been sunk, Congratulations!")
-        board[user_row_guess][user_column_guess] = "/"
+# Encapsulated the game into a class for easier user functionality
+class Battleships:
+    # Generates board, places ships on the board, and provides the amount of guesses
+    def __init__(self):
+        self.board = [["."] * 5 for _ in range(5)]
+        self.ships = self.place_ships()
+        self.max_attempts = 5
+        self.attempts = 0
         
-    # If the user guesses the incorrect coordiates of the ship they'll be informed of this with an "X" displayed on the board
-    else:
-        print("No hit! Better luck next time.")
-        board[user_row_guess][user_column_guess] = "X"
+    # Displays welcome message with instructions on how to play
+    def welcome(self):
+        print("Hello and welcome to my Battleships game!")
+        print("You will have 5 ships across a 5X5 board.")
+        print("You will have 5 guesses to guess the locations of the ships!")
+        print("Ready.. set.. GO!")
+        print(" ")
+
+    # Displays the board to the terminal
+    def print_board(self):
+        for row in self.board:
+            print(" ".join(row))
+
+    # Places the ships on the board and generates the computers location
+    def place_ships(self):
+        ships = set()
+        while len(ships) < 5:
+            row = random.randint(0, 4)
+            col = random.randint(0, 4)
+            ships.add((row, col))
+        return ships
+
+    # Prints location of the ships locations for so the assessor can check for validity
+    def print_ship_locations(self):
+        print("Current ship locations for validation:")
+        for ship in self.ships:
+            print(ship)
+        print(" ")
+
+    # Urges the user to type their guesses for the ships to the board
+    def guess_entry(self):
+        while True:
+            try:
+                # Asks the user to guess the row they believe the ship is on
+                user_row_guess = int(input("Enter a Row Number: "))
+                
+                # Asks the user to guess the row they believe the ship is on
+                user_column_guess = int(input("Enter a Column Number: "))
+                
+                # If invalid output is placed on the board the user will be directed to use the correct integers
+                if 0 <= user_row_guess <= 4 and 0 <= user_column_guess <= 4:
+                    return user_row_guess, user_column_guess
+                else:
+                    print("Please enter numbers between 0 and 4.")
+            except ValueError:
+                print("Invalid input. Please enter integers only.")
+
+    # Validated the users output
+    def validate_location_guess(self, user_row_guess, user_column_guess):
+        if self.board[user_row_guess][user_column_guess] in ["X", "/"]:
             
-    print_board(board)
-    
-# Creates the loop so the player can play and updates the board after each play    
-def game_over():
-    game_over = False
-    attempts = 0
-    max_attempts = 4
-    while not game_over and attempts < max_attempts:
-        guess_location()
-        user_row_guess, user_column_guess = guess_entry()
-        game_over = validate_location_guess(user_row_guess, user_column_guess)
-        attempts += 1 
-        
-# Functions added to main for easier log of called functions
-def main():
-    welcome()
-    # Computers guess has been commented out but can be displayed by the assessor so they can see the result if the correct coordinates are entered
-    #guess_location()
-    user_row_guess, user_column_guess = guess_entry()
-    validate_location_guess(user_row_guess, user_column_guess)
-    game_over()
-    print("All attempts exceeded, Game Over!")
+            # If the location has already been guessed the user will have another chance to guess
+            print("You've already guessed that location! Try again.")
+            return False
 
-# Calls main function
-if __name__ == "__main__":     
+        # When the users guess is correct they'll receive a message advising of this
+        if (user_row_guess, user_column_guess) in self.ships:
+            print("Battleship has been sunk, Congratulations!")
+            self.board[user_row_guess][user_column_guess] = "/"
+            self.ships.remove((user_row_guess, user_column_guess))  # Remove the sunk ship
+            self.print_board()
+            return True
+        
+        # When the users guess is incorrect they'll receive a message advising of this
+        else:
+            print("No hit! Better luck next time.")
+            self.board[user_row_guess][user_column_guess] = "X"
+            self.print_board()
+            return False
+
+    def play_game(self):
+        self.welcome()
+        
+        # Print ship locations for the user
+        self.print_ship_locations()
+        
+        # Allows the user to guess as long as the number of attempts is below the max amount
+        while self.attempts < self.max_attempts and self.ships:
+            user_row_guess, user_column_guess = self.guess_entry()
+            if self.validate_location_guess(user_row_guess, user_column_guess):
+                # If all ships are sunk
+                if not self.ships:  
+                    print("You got them all, you won!")
+                    return
+            self.attempts += 1
+        # Lets the user know the game is over
+        print("Game Over!")
+        
+# Calls the functions in the main function
+def main():
+    game = Battleships()
+    game.play_game()
+
+if __name__ == "__main__":
     main()
